0a1a8b53a056004d877e313439b8a3b99148711b
Sebastian Ojefors <godnoken@gmail.com>
2022-03-10 21:22:05 +0100

    Add error messages and aggressive/passive error visuals
    Aggressive/passive errors. Passive means that errors won't show until
    users exits focus on an input and aggressive means that positive
    feedback shows as soon as input is valid

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ const formInputs = Array.from(document.querySelectorAll(".input")).slice(0, 4);
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPassword");
 const passwordInputs = Array.from([password, confirmPassword]);
+const passwordError = document.querySelector("#passwordError");
 
 
 form.addEventListener("submit", validateForm);
-formInputs.forEach(input => input.addEventListener("input", validateInput));
-passwordInputs.forEach(input => input.addEventListener("input", validatePassword));
+formInputs.forEach(input => input.addEventListener("change", validateOnChange));
+formInputs.forEach(input => input.addEventListener("input", validateOnInput));
+passwordInputs.forEach(input => input.addEventListener("input", validatePasswordOnInput));
+passwordInputs.forEach(input => input.addEventListener("change", validatePasswordOnChange));
 
 
 function validateForm(event) {
-    if (form.checkValidity() && validatePassword()) {
+    if (form.checkValidity() && validatePasswordOnInput()) {
         event.preventDefault()
-
     }
     else {
         event.preventDefault();
@@ -30,38 +32,66 @@ function validateForm(event) {
             }
         })
 
-        validatePassword();
+        validatePasswordOnInput();
+        validatePasswordOnChange();
     }
 }
 
-
-function validateInput(event) {
+// Separation of onInput and onChange made to adhere to agressive-passive
+// behaviour on error/validation
+function validateOnInput(event) {
     const targetInput = event.target;
+    const errorMessage = targetInput.nextElementSibling;
 
     if (targetInput.checkValidity()) {
         targetInput.classList.add("border-green-600");
         targetInput.classList.remove("border-red-600");
+        errorMessage.classList.add("invisible");
+        errorMessage.classList.remove("visible");
     }
-    else {
-        targetInput.classList.add("border-red-600");
-        targetInput.classList.remove("border-green-600");
-    }
-
 }
 
 
-function validatePassword() {
+function validateOnChange(event) {
+    const targetInput = event.target;
+    const errorMessage = targetInput.nextElementSibling;
+
+    if (!targetInput.checkValidity()) {
+        targetInput.classList.add("border-red-600");
+        targetInput.classList.remove("border-green-600");
+        errorMessage.classList.add("visible");
+        errorMessage.classList.remove("invisible");
+    }
+}
+
+
+function validatePasswordOnInput() {
     if (password.value === confirmPassword.value && password.value.length >= 8) {
         password.classList.add("border-green-600");
         confirmPassword.classList.add("border-green-600");
         password.classList.remove("border-red-600");
         confirmPassword.classList.remove("border-red-600");
+        passwordError.classList.remove("visible");
+        passwordError.classList.add("invisible");
         return true;
     }
-    else {
+}
+
+function validatePasswordOnChange() {
+    if (password.value !== confirmPassword.value || password.value.length < 8) {
         password.classList.add("border-red-600");
         confirmPassword.classList.add("border-red-600");
         password.classList.remove("border-green-600");
         confirmPassword.classList.remove("border-green-600");
+        passwordError.classList.add("visible");
+        passwordError.classList.remove("invisible");
+        passwordError.textContent = "Password does not match and is less than 8 characters long";
+
+        if (password.value !== confirmPassword.value && password.value.length >= 8) {
+            passwordError.textContent = "Password does not match";
+        }
+        else if (password.value === confirmPassword.value && password.value.length < 8) {
+            passwordError.textContent = "Password is less than 8 characters long"
+        }
     }
 }
