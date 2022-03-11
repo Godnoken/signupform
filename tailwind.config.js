82e79b467d03d10ac437e2392adbd8aedd02be93
Sebastian Ojefors <godnoken@gmail.com>
2022-03-12 00:37:23 +0100

    Add animation to title

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         formError: "clamp(12px, 1 * (1vw + 1vh) / 2, 100px)",
         desktopMain: "clamp(12px, 2 * (1vw + 1vh) / 2, 100px)",
         desktopForm: "clamp(12px, 1 * (1vw + 1vh) / 2, 100px)",
+      },
+      backgroundImage: {
+        heroTitle: "linear-gradient(to left, #970076 50%, black 50%)",
+        heroTitleDark: "linear-gradient(to left, #ff46d7 50%, #f8f8ff 50%)"
       }
     },
   },
