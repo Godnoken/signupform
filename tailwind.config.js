ff2cf29f52f7f2a9c953ae463a2b212f08f1f21f
Sebastian Ojefors <godnoken@gmail.com>
2022-03-11 23:25:13 +0100

    Change fonts to a minimum of 12px to be legible

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        main: "clamp(10px, 5 * (1vw + 1vh) / 2, 100px)",
-        form: "clamp(10px, 2 * (1vw + 1vh) / 2, 100px)",
-        formError: "clamp(10px, 1 * (1vw + 1vh) / 2, 100px)",
-        desktopMain: "clamp(10px, 2 * (1vw + 1vh) / 2, 100px)",
-        desktopForm: "clamp(10px, 1 * (1vw + 1vh) / 2, 100px)",
+        main: "clamp(12px, 5 * (1vw + 1vh) / 2, 100px)",
+        form: "clamp(12px, 2 * (1vw + 1vh) / 2, 100px)",
+        formError: "clamp(12px, 1 * (1vw + 1vh) / 2, 100px)",
+        desktopMain: "clamp(12px, 2 * (1vw + 1vh) / 2, 100px)",
+        desktopForm: "clamp(12px, 1 * (1vw + 1vh) / 2, 100px)",
       }
     },
   },
