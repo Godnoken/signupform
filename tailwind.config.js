4c40167db744423e146adc28d5f884e7a53ffc34
Sebastian Ojefors <godnoken@gmail.com>
2022-03-11 22:23:21 +0100

    Add fully responsive desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      fontSize: {
+        main: "clamp(10px, 5 * (1vw + 1vh) / 2, 100px)",
+        form: "clamp(10px, 2 * (1vw + 1vh) / 2, 100px)",
+        formError: "clamp(10px, 1 * (1vw + 1vh) / 2, 100px)",
+        desktopMain: "clamp(10px, 2 * (1vw + 1vh) / 2, 100px)",
+        desktopForm: "clamp(10px, 1 * (1vw + 1vh) / 2, 100px)",
+      }
     },
   },
   plugins: [
